1c61fcf83e2c953c853a00b8dcc8ed0cd1da7227
kazunori <gaogaigar673@gmail.com>
2021-04-22 19:43:51 +0900

    [Finish]js 3章

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
+}
 
 var js_hand = getJShand();
-
 var judge = winLose(user_hand, js_hand);
 
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert('またチャレンジしてね')
+}
 
 function getJShand(){
   var js_hand_num = Math.floor( Math.random() * 3 );
